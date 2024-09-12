76c1000d77c3b434b727a1e19ca290c6626890cf
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-12 09:25:33 +0530

    project init

---
+++ main.py
@@ -0,0 +1,38 @@
+print("hello world")
+
+if 5 > 2:
+  print("Five is greater than two!")
+else:
+   print("wrong!")   
+   
+x = 5
+y = "John"
+print(x)
+x=7
+print(x)
+x=y
+print(x)
+print(y)
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+
+
+  
+  
+  
+  # def my_function():
+  # print("Hello from a function")
+  # my_function()
+  
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Emil")
+
+
